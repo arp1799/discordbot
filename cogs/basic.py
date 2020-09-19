8fc47c96f5616b5182bdadf78e60fc00fba60bc0
agrawalarpit1799@gmail.com <you@example.com>
2020-09-19 21:23:07 +0530

    add basic.py

---
+++ cogs/basic.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+from utils import text_to_owo
+
+class Common(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+    
+
+    @commands.command(name="owo",help="Converts text to owo text")
+    async def owo(self,ctx):
+        text=text_to_owo(ctx.message.content)
+        await ctx.send(text)
+    
+    @commands.command(name="invite",help="Creates invite link valid for 1 day")
+    async def invite(self,ctx):
+        link=await ctx.channel.create_invite(max_age=1)
+        await ctx.send(link)
+
+def setup(bot):
+    bot.add_cog(Common(bot))
