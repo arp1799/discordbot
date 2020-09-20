413b622daab19c5bd6b2bb04e88406ffe141f4ce
Leonidas328 <arjunsaini328@gmail.com>
2020-09-20 19:58:17 +0530

    Added functionality for playing music

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ import random
 from dotenv import load_dotenv
 from discord.ext import commands
 from settings import *
+import youtube_dl
+
 print(DTOKEN)
 load_dotenv()
 
@@ -17,6 +19,71 @@ bot=commands.Bot(command_prefix='#')
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py") and filename != "__init__.py":
         bot.load_extension(f'cogs.{filename[:-3]}')
+
+#-----------Class+Stuff needed for Playing Music----------
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+#-----------Playing Music------------
+@bot.command(name='voice')
+async def join_voice(context):
+    connected = context.author.voice
+    url = context.message.content.strip("#voice ")
+    if connected:
+        try:
+            vc = await connected.channel.connect()
+        except:
+            pass
+        player = await YTDLSource.from_url(url, loop=bot.loop)
+        vc.play(player, after=lambda e: disconnect_after_played(context, vc))
+        await context.send('Now playing: {}'.format(player.title))
+    else:
+        response = "Plese join a voice channel first."
+        await context.send(response)
+
+
 
 #-----------exit command-------------
 @bot.command(name='exit',help="exiting bot")
